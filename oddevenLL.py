f2185a13827c11c3c20e77ce6be0cc6ec15500f7
Saul385 <84159357+Saul999@users.noreply.github.com>
2023-11-29 20:28:25 -0800

    oddeven solution

---
+++ oddevenLL.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        odd = head
+        even = even_head = head.next
+
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+
+            odd = odd.next
+            even = even.next
+
+        odd.next = even_head
+
+        return head
