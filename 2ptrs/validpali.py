2a91ebe348f2dea2e8c9e937b8e070046549be1a
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-01-12 10:41:08 -0800

    289/485 first attempt

---
+++ 2ptrs/validpali.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        s = s.replace(" ", "")
+        s = s.replace(",", "")
+        s = s.replace(":", "")
+        s = s.replace(".", "")
+
+        print(s)
+        if len(s) == 0 or len(s) == 1:
+            return True
+
+        leftptr = 0
+        print(leftptr)
+        rightptr = len(s) - 1
+        print(rightptr)
+        for i in range(len(s) - 1):
+            if leftptr >= rightptr:
+                return True
+            if s[leftptr] == s[rightptr]:
+                print(s[rightptr])
+                print(s[leftptr])
+                rightptr -= 1
+                leftptr += 1
+            else:
+                return (False)
