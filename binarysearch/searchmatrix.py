04e16047c98086e277f68611fb98c9825eedb410
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-01-17 11:13:16 -0800

    started not finsihed

---
+++ binarysearch/searchmatrix.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+
+        rows = len(matrix[0])  # 4
+        cols = len(matrix)  # 3
+
+        finalval = 0
+        while finalval != target:
+            colCounter = -1
+            rowCounter = -1
+            if cols[colCounter] > target:
+                colCounter -= 1
+            elif rows[rowCounter] > target:
+                rowCounter -= 1
+            elif row[rowCounter] == target:
+                finalval = target
+                return rows, cols
