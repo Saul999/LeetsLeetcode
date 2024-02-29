cd23d1e4390e6ce5cd5e49d5b5fe63de45626033
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-28 21:55:15 -0800

    completed optimal solution

--- 2ptrs/moveZeros.py
+++ 2ptrs/moveZeros.py
@@ -11,40 +11,44 @@ class Solution(object):
         #leftptr = 1
         #rightptr = 4
         leftptr = 0
-        rightptr = 1
-        leftval = 0
-        rightval = 0
+        # rightptr = 1
+        # leftval = 0
+        # rightval = 0
         
+        for rightptr in range(len(nums)):
+            if nums[rightptr] != 0:
+                nums[rightptr], nums[leftptr] = nums[leftptr], nums[rightptr]
+                leftptr += 1
+
+        return nums
+
+
 
         #2 ptrs move together and swapping values
-        for i in range(len(nums)):
-            # print('right ' + str(rightptr) +' '+ str(nums[rightptr]))
-            # print("left " + str(leftptr) + " " + str(nums[leftptr]))
-            if rightptr == len(nums):
-                break
-            if nums[leftptr] == 0 and nums[rightptr] == 0 and (rightptr + 1) < len(nums):
-                leftval = nums[leftptr]
-                rightval = nums[rightptr + 1]
-                nums[rightptr + 1] = leftval
-                nums[leftptr] = rightval   
-                if nums[leftptr] == 0 and (rightptr + 2) < len(nums):
-                    leftval = nums[leftptr]
-                    rightval = nums[rightptr + 2]
-                    nums[rightptr + 2] = leftval
-                    nums[leftptr] = rightval  
-            if nums[leftptr] == 0:
-                leftval = nums[leftptr]
-                rightval = nums[rightptr]
-                nums[leftptr] = rightval
-                nums[rightptr] = leftval
+        # for i in range(len(nums)):
+        #     # print('right ' + str(rightptr) +' '+ str(nums[rightptr]))
+        #     # print("left " + str(leftptr) + " " + str(nums[leftptr]))
+        #     if rightptr == len(nums):
+        #         break
+        #     if nums[leftptr] == 0 and nums[rightptr] == 0 and (rightptr + 1) < len(nums):
+        #         leftval = nums[leftptr]
+        #         rightval = nums[rightptr + 1]
+        #         nums[rightptr + 1] = leftval
+        #         nums[leftptr] = rightval   
+                
+        #     if nums[leftptr] == 0:
+        #         leftval = nums[leftptr]
+        #         rightval = nums[rightptr]
+        #         nums[leftptr] = rightval
+        #         nums[rightptr] = leftval
             
-            leftptr +=1
-            rightptr += 1
+        #     leftptr +=1
+        #     rightptr += 1
 
         # new solution throw 0 to the back
         # for i in range(len(nums)):
         #     if nums[leftptr] == 0:
 
 
-        print(nums)
-        return nums
+     
+        
