8e567f527bb5715309c5d6aeb04e28aa851f513b
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-03-07 18:02:07 -0800

    not optimized but passed all test *redo*

--- arrays/2215Difof2Arrays.py
+++ arrays/2215Difof2Arrays.py
@@ -7,8 +7,8 @@ class Solution(object):
         """
         mapped = {}
         final = [[],[]]
-        first = []
-        second = [] 
+        first = set()
+        second = set()
         
         for i in nums1:
             if i in nums2:
@@ -17,20 +17,18 @@ class Solution(object):
         for i in range(len(nums1)):
             print(i)
             print("nums1 " + str(nums1[i]))
-            if nums1[i] in mapped and nums2[i] in mapped:
-                print("both worked")
+            if nums1[i] in mapped:
                 continue
-            elif nums1[i] in mapped and nums2[i] not in second:
-                print("nums1 worked")
-                second.append(nums2[i])
-            elif nums2[i] in mapped and nums1[i] not in first:
-                print("nums2 worked")
-                first.append(nums1[i])
             else:
-                first.append(nums1[i])
-                second.append(nums2[i])
+                first.add(nums1[i])
 
-            
+        for i in range(len(nums2)):
+            print(i)
+            print("nums2 " + str(nums2[i]))
+            if nums2[i] in mapped:
+                continue
+            else:
+                second.add(nums2[i])
 
         final = [first, second]
         print(mapped)
