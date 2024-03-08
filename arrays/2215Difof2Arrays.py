38e61731093bc9d6d1d4af9451703109bd042809
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-03-07 18:12:10 -0800

    fastest without built in functions or one liners

--- arrays/2215Difof2Arrays.py
+++ arrays/2215Difof2Arrays.py
@@ -5,34 +5,14 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[List[int]]
         """
-        mapped = {}
-        final = [[],[]]
         first = set()
         second = set()
         
         for i in nums1:
-            if i in nums2:
-                mapped[i] = i
-        
-        for i in range(len(nums1)):
-            print(i)
-            print("nums1 " + str(nums1[i]))
-            if nums1[i] in mapped:
-                continue
-            else:
-                first.add(nums1[i])
-
-        for i in range(len(nums2)):
-            print(i)
-            print("nums2 " + str(nums2[i]))
-            if nums2[i] in mapped:
-                continue
-            else:
-                second.add(nums2[i])
-
-        final = [first, second]
-        print(mapped)
-        return final
+            if i not in nums2:
+                first.add(i)
+        for i in nums2:
+            if i not in nums1:
+                second.add(i)
                 
-        
-                
+        return[first, second]
