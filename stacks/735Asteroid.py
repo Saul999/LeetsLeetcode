e3b3281dfb94977c43ae228937f63c14c4e5d7c6
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-03-07 18:48:33 -0800

    reattempted 3/7 optimal

--- stacks/735Asteroid.py
+++ stacks/735Asteroid.py
@@ -1,3 +1,6 @@
+#REATTEMPT FROM  3/7/24 PASSED MOST OPTIMAL SOLUTION
+
+
 class Solution(object):
     def asteroidCollision(self, asteroids):
         """
@@ -5,20 +8,50 @@ class Solution(object):
         :rtype: List[int]
         """
         stack = []
-
-        for a in asteroids:
-            if a > 0:
-                stack.append(a)
-            else:
+        for i in range(len(asteroids)):
+            if asteroids[i] > 0:
+                stack.append(asteroids[i])
+            if asteroids[i] < 0:
                 while stack and stack[-1] > 0:
-                    if stack[-1] == abs(a):
+                    if (abs(asteroids[i]) > abs(stack[-1])):
+                        stack.pop()
+                    elif abs(asteroids[i]) == abs(stack[-1]):
                         stack.pop()
                         break
-                    elif stack[-1] < abs(a):
-                        stack.pop()
-                    else:
+                    else: 
                         break
+                    
                 else:
-                    stack.append(a)
+                    stack.append(asteroids[i])
+                    
+            
 
         return stack
+
+
+# class Solution(object):
+#     def asteroidCollision(self, asteroids):
+#         """
+#         :type asteroids: List[int]
+#         :rtype: List[int]
+#         """
+#         stack = []
+
+#         for a in asteroids:
+#             if a > 0:
+#                 stack.append(a)
+#             else:
+#                 while stack and stack[-1] > 0:
+#                     if stack[-1] == abs(a):
+#                         stack.pop()
+#                         break
+#                     elif stack[-1] < abs(a):
+#                         stack.pop()
+#                     else:
+#                         break
+#                 else:
+#                     stack.append(a)
+
+        # return stack
+
+    
