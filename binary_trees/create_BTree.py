1e49f1dae1faca52b4deb335533d94286b4a033d
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-03-27 18:10:24 -0700

    bfs added

--- binary_trees/create_BTree.py
+++ binary_trees/create_BTree.py
@@ -4,12 +4,12 @@ class Node:
         self.left = None
         self.right = None
 
-a = Node('a')
-b = Node('b')
-c = Node('c')
-d = Node('d')
-e = Node('e')
-f = Node('f')
+a = Node(20)
+b = Node(5)
+c = Node(3)
+d = Node(9)
+e = Node(10)
+f = Node(6)
 
 a.right = c
 a.left = b
@@ -30,26 +30,48 @@ def dfs(root):
     if root == None:
         return []
     stack = [root]
-    res = []
+    res = 0
     while stack:
         cur = stack.pop()
-        res.append(cur)
-        print(cur.val)
+        res += cur.val
         if cur.right:
             stack.append(cur.right)
         if cur.left:
             stack.append(cur.left)
-        
+    print(res)
+    return res
 
 def dfs_recursion(root):
     if root:
+        if root.val == 'e':
+            return True
         dfs(root.left)
-        print(root.val)
         dfs(root.right)
+    return False
+
+
+# gets min and returns it
+def bfs(root):
+    if root == None:
+        return []
+    queue = [root]
+    min = 10000
+    while queue:
+        cur = queue.pop(0)
+        if cur.val < min:
+            copy = cur
+            min = cur.val
+        if cur.left:
+            queue.append(cur.left)
+        if cur.right:
+            queue.append(cur.right)
+    print(min)
+    return False
     
-        
 
-dfs_recursion(a)
-
+# dfs_recursion(a)
+bfs(a)
+# dfs_recursion(a)
+dfs(a)
 
 
