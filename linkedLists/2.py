050e18ce1411230bf098bdfbd1ed2bf0baa1e75a
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-06-03 14:35:17 -0700

    redo again

--- linkedLists/2.py
+++ linkedLists/2.py
@@ -10,29 +10,35 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        #creates new LL
         res = ListNode()
+        #creates copy 
         cur = res
         carry = 0
 
         while l1 or l2 or carry:
             if l1:
-                val1 = l1.val
-            else: 
-                val1 = 0
-            if l2:
-                val2 = l2.val
+                l1_val = l1.val
             else:
-                val2 = 0
-
+                l1_val = 0
+            if l2:
+                l2_val = l2.val
+            else:
+                l2_val = 0
             
-            val = val1 + val2 + carry
+            val = l1_val + l2_val + carry
             carry = val // 10
             val = val % 10
-            cur.next = ListNode(val) 
-            cur = cur.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-
-        return res.next
             
+            cur.next = ListNode(val)
+            cur = cur.next
+            #moves l1 and l2 to next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+            
+        return res.next
+
+
 
