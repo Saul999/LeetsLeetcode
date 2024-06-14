46baaea5f38ab107c5a36d4da54ae4c4448a0618
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-06-14 15:08:17 -0700

    solved

--- twosum.py
+++ twosum.py
@@ -1,3 +1,26 @@
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         # holder = {}
+
+        # #
+        # #
+        # #
+        # #
+        # index = 0
+
+        # for i in range(len(nums)):
+        #     ans = target - nums[i]
+        #     if ans in holder:
+        #         return holder[ans], i
+        #     else:
+        #         holder[nums[i]] = i
+
+        #FINAL ATTEMPT NO HELP
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -5,17 +28,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        holder = {}
-
-        #
-        #
-        #
-        #
-        index = 0
+        values = {}
 
         for i in range(len(nums)):
-            ans = target - nums[i]
-            if ans in holder:
-                return holder[ans], i
+            value = target - nums[i]
+            if value in values:
+                return [values[value],i]
             else:
-                holder[nums[i]] = i
+                values[nums[i]] = i
+
