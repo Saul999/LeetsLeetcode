2a50166634455b6ace2a496221a742532d09369f
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-07-08 13:00:55 -0700

    finished

--- windowsThatSlide/B_and_S_Stock.py
+++ windowsThatSlide/B_and_S_Stock.py
@@ -19,6 +19,32 @@ class Solution(object):
             right += 1
 
         return maxSell
+
+# class Solution(object):
+#     def maxProfit(self, prices):
+#         """
+#         :type prices: List[int]
+#         :rtype: int
+#         """
+#         max_val = 0
+
+#         l, r = 0, 1
+        
+#         while r <= len(prices) - 1:
+#             if prices[r] < prices[l]:
+#                 l = r
+#                 r += 1
+#             else:
+#                 val = prices[r] - prices[l]
+#                 max_val = max(max_val, val)
+#                 r += 1
+#         return max_val            
+
+
+                
+            
+
+
             
             
                 
