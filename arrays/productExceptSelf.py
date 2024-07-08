36422c650e338bcf95c1ce4ba932889c435e2d4c
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-07-08 13:40:53 -0700

    finished

--- arrays/productExceptSelf.py
+++ arrays/productExceptSelf.py
@@ -1,31 +1,50 @@
+# class Solution(object):
+#     def productExceptSelf(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
+#         n = len(nums)
+#         pre, post = [1] * n, [1]* n
+#         sumPre = 1
+#         sumPost = 1
+#         final = [1] * n
+#         print(len(pre),post)
+
+
+#         for i in range(n):
+#             final[i] *= sumPre
+#             sumPre = sumPre * nums[i]
+
+#         for i in range(len(nums)-1, -1, -1):
+#             final[i] *= sumPost
+#             sumPost = sumPost * nums[i]
+
+#         # for i in range(n):
+#         #     final[i]= pre[i] * post[i]
+
+#         print(final)
+#         print(pre)
+#         print(post)
+#         return final
+
+#final answer 7/08/24
 class Solution(object):
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        pre, post = [1] * n, [1]* n
-        sumPre = 1
-        sumPost = 1
-        final = [1] * n
-        print(len(pre),post)
+        final = [1] * (len(nums))
+        product = 1
+        for i in range(len(nums)):
+            final[i] = product
+            product = nums[i] * product 
+        product2 = 1
+        for i in range(len(nums) -1, -1,-1):
+            final[i] = product2 * final[i]
+            product2 = nums[i] * product2
 
-
-        for i in range(n):
-            final[i] *= sumPre
-            sumPre = sumPre * nums[i]
-
-        for i in range(len(nums)-1, -1, -1):
-            final[i] *= sumPost
-            sumPost = sumPost * nums[i]
-
-        # for i in range(n):
-        #     final[i]= pre[i] * post[i]
-
-        print(final)
-        print(pre)
-        print(post)
         return final
 
         
